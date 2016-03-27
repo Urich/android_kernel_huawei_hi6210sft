9421996012ae6b6058d22e9ea45a21f4b6aa01fc
lloco73 <lloco73@gmail.com>
2016-03-27 21:26:35 +0100

    governor added

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 27 20:26:51 BST 2016"
+#define LINUX_COMPILE_BY "lloco73"
+#define LINUX_COMPILE_HOST "lloco73"
+#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.86-gcf53229-dirty"
